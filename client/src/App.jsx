ba8831314b6979c0431b4636cb6ffba1a70de3f3
Raj Yadav <rajyadav7047@gmail.com>
2024-11-03 21:20:33 +0530

    second commit for building client part

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import AuthLayout from "./components/auth/layout";
+import LogIn from "./pages/auth/login";
+import Register from "./pages/auth/register";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div className="flex flex-col overflow-hidden bg-white">
+      <h1>Header Component</h1>
 
-export default App
+      <Routes>
+        <Route path="/auth" element={<AuthLayout />}>
+          <Route path="login" element={<LogIn />} />
+          <Route path="register" element={<Register />} />
+        </Route>
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
